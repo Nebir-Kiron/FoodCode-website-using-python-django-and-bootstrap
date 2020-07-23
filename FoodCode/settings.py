bb425273f5193e6b1162f6390db1cef4dac417fc
Nebir Kiron <kironnibir046@gmail.com>
2020-07-23 10:43:30 +0600

    connected to mysql and created the food app

--- FoodCode/settings.py
+++ FoodCode/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'FoodCode.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'FoodCode',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
