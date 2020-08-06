323e8111f50e14a272e84e6d461878f57bd7f90f
Nebir Kiron <kironnibir046@gmail.com>
2020-08-06 07:36:24 +0600

    updated models templates

--- food/models.py
+++ food/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Item(models.Model):
+    item_name = models.CharField(max_length=100)
+    item_desc = models.CharField(max_length=500)
+    item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500,default="https://image.freepik.com/free-vector/fast-food-vector-card-with-text-placeholder_78458-98.jpg")
+
+    def __str__(self):
+        return self.item_name
+    
--- food/views.py
+++ food/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from . models import Item
 # Create your views here.
+def index(request):
+    item_list = Item.objects.all()
+    contex = {
+        'item_list':item_list,
+    }
+    return render(request,'food/index.html',contex)
+
+def item(request):
+    return HttpResponse("Item view")
+
+def details(request,id):
+    item = Item.objects.get(id=id)
+    contex = {
+        'item':item,
+    }
+    return render(request,'food/details.html',contex)
